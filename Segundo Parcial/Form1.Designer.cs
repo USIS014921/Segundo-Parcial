e75048c00e84368b06e53873ecff50c991b2c523
Joan Steven Gómez Contreras <joanstevengomezcontreras@outlook.com>
2021-10-20 11:37:37 -0600

    Agregar archivos de proyecto.

---
+++ Segundo Parcial/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace Segundo_Parcial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bguardar = new System.Windows.Forms.Button();
+            this.beliminar = new System.Windows.Forms.Button();
+            this.bmodificar = new System.Windows.Forms.Button();
+            this.bentrar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bbuscar = new System.Windows.Forms.Button();
+            this.bsalir = new System.Windows.Forms.Button();
+            this.txtbuscar = new System.Windows.Forms.TextBox();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.txtclave = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bnuevo = new System.Windows.Forms.Button();
+            this.lstnivel = new System.Windows.Forms.ComboBox();
+            this.bactualizar = new System.Windows.Forms.Button();
+            this.bprimero = new System.Windows.Forms.Button();
+            this.banterior = new System.Windows.Forms.Button();
+            this.bsiguiente = new System.Windows.Forms.Button();
+            this.bultimo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bguardar
+            // 
+            this.bguardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bguardar.Location = new System.Drawing.Point(11, 177);
+            this.bguardar.Name = "bguardar";
+            this.bguardar.Size = new System.Drawing.Size(80, 30);
+            this.bguardar.TabIndex = 1;
+            this.bguardar.Text = "Guardar";
+            this.bguardar.UseVisualStyleBackColor = true;
+            this.bguardar.Click += new System.EventHandler(this.bguardar_Click);
+            // 
+            // beliminar
+            // 
+            this.beliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beliminar.Location = new System.Drawing.Point(97, 177);
+            this.beliminar.Name = "beliminar";
+            this.beliminar.Size = new System.Drawing.Size(80, 30);
+            this.beliminar.TabIndex = 2;
+            this.beliminar.Text = "Eliminar";
+            this.beliminar.UseVisualStyleBackColor = true;
+            this.beliminar.Click += new System.EventHandler(this.beliminar_Click);
+            // 
+            // bmodificar
+            // 
+            this.bmodificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bmodificar.Location = new System.Drawing.Point(184, 177);
+            this.bmodificar.Name = "bmodificar";
+            this.bmodificar.Size = new System.Drawing.Size(80, 30);
+            this.bmodificar.TabIndex = 3;
+            this.bmodificar.Text = "Modificar";
+            this.bmodificar.UseVisualStyleBackColor = true;
+            this.bmodificar.Click += new System.EventHandler(this.bmodificar_Click);
+            // 
+            // bentrar
+            // 
+            this.bentrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bentrar.Location = new System.Drawing.Point(269, 177);
+            this.bentrar.Name = "bentrar";
+            this.bentrar.Size = new System.Drawing.Size(80, 30);
+            this.bentrar.TabIndex = 4;
+            this.bentrar.Text = "Entrar";
+            this.bentrar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 213);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(540, 218);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // bbuscar
+            // 
+            this.bbuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bbuscar.Location = new System.Drawing.Point(355, 141);
+            this.bbuscar.Name = "bbuscar";
+            this.bbuscar.Size = new System.Drawing.Size(80, 30);
+            this.bbuscar.TabIndex = 6;
+            this.bbuscar.Text = "Buscar";
+            this.bbuscar.UseVisualStyleBackColor = true;
+            this.bbuscar.Click += new System.EventHandler(this.bbuscar_Click);
+            // 
+            // bsalir
+            // 
+            this.bsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bsalir.Location = new System.Drawing.Point(355, 177);
+            this.bsalir.Name = "bsalir";
+            this.bsalir.Size = new System.Drawing.Size(80, 30);
+            this.bsalir.TabIndex = 7;
+            this.bsalir.Text = "Salir";
+            this.bsalir.UseVisualStyleBackColor = true;
+            this.bsalir.Click += new System.EventHandler(this.bsalir_Click);
+            // 
+            // txtbuscar
+            // 
+            this.txtbuscar.Location = new System.Drawing.Point(441, 148);
+            this.txtbuscar.Name = "txtbuscar";
+            this.txtbuscar.Size = new System.Drawing.Size(100, 20);
+            this.txtbuscar.TabIndex = 8;
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(255, 12);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(100, 20);
+            this.txtusuario.TabIndex = 9;
+            // 
+            // txtclave
+            // 
+            this.txtclave.Location = new System.Drawing.Point(255, 39);
+            this.txtclave.Name = "txtclave";
+            this.txtclave.Size = new System.Drawing.Size(100, 20);
+            this.txtclave.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(181, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(181, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Clave:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(181, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Nivel:";
+            // 
+            // bnuevo
+            // 
+            this.bnuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bnuevo.Location = new System.Drawing.Point(11, 177);
+            this.bnuevo.Name = "bnuevo";
+            this.bnuevo.Size = new System.Drawing.Size(80, 30);
+            this.bnuevo.TabIndex = 15;
+            this.bnuevo.Text = "Nuevo";
+            this.bnuevo.UseVisualStyleBackColor = true;
+            this.bnuevo.Click += new System.EventHandler(this.bnuevo_Click);
+            // 
+            // lstnivel
+            // 
+            this.lstnivel.FormattingEnabled = true;
+            this.lstnivel.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.lstnivel.Location = new System.Drawing.Point(255, 69);
+            this.lstnivel.Name = "lstnivel";
+            this.lstnivel.Size = new System.Drawing.Size(100, 21);
+            this.lstnivel.TabIndex = 16;
+            // 
+            // bactualizar
+            // 
+            this.bactualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bactualizar.Location = new System.Drawing.Point(184, 177);
+            this.bactualizar.Name = "bactualizar";
+            this.bactualizar.Size = new System.Drawing.Size(80, 30);
+            this.bactualizar.TabIndex = 17;
+            this.bactualizar.Text = "Actualizar";
+            this.bactualizar.UseVisualStyleBackColor = true;
+            this.bactualizar.Click += new System.EventHandler(this.bactualizar_Click);
+            // 
+            // bprimero
+            // 
+            this.bprimero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bprimero.Location = new System.Drawing.Point(11, 141);
+            this.bprimero.Name = "bprimero";
+            this.bprimero.Size = new System.Drawing.Size(80, 30);
+            this.bprimero.TabIndex = 18;
+            this.bprimero.Text = "Primero";
+            this.bprimero.UseVisualStyleBackColor = true;
+            // 
+            // banterior
+            // 
+            this.banterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.banterior.Location = new System.Drawing.Point(97, 141);
+            this.banterior.Name = "banterior";
+            this.banterior.Size = new System.Drawing.Size(80, 30);
+            this.banterior.TabIndex = 19;
+            this.banterior.Text = "Anterior";
+            this.banterior.UseVisualStyleBackColor = true;
+            // 
+            // bsiguiente
+            // 
+            this.bsiguiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bsiguiente.Location = new System.Drawing.Point(184, 141);
+            this.bsiguiente.Name = "bsiguiente";
+            this.bsiguiente.Size = new System.Drawing.Size(80, 30);
+            this.bsiguiente.TabIndex = 20;
+            this.bsiguiente.Text = "Siguiente";
+            this.bsiguiente.UseVisualStyleBackColor = true;
+            this.bsiguiente.Click += new System.EventHandler(this.bsiguiente_Click);
+            // 
+            // bultimo
+            // 
+            this.bultimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bultimo.Location = new System.Drawing.Point(269, 141);
+            this.bultimo.Name = "bultimo";
+            this.bultimo.Size = new System.Drawing.Size(80, 30);
+            this.bultimo.TabIndex = 21;
+            this.bultimo.Text = "Ultimo";
+            this.bultimo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(564, 441);
+            this.Controls.Add(this.bultimo);
+            this.Controls.Add(this.bsiguiente);
+            this.Controls.Add(this.banterior);
+            this.Controls.Add(this.bprimero);
+            this.Controls.Add(this.bactualizar);
+            this.Controls.Add(this.lstnivel);
+            this.Controls.Add(this.bnuevo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtclave);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.txtbuscar);
+            this.Controls.Add(this.bsalir);
+            this.Controls.Add(this.bbuscar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.bentrar);
+            this.Controls.Add(this.bmodificar);
+            this.Controls.Add(this.beliminar);
+            this.Controls.Add(this.bguardar);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button bguardar;
+        private System.Windows.Forms.Button beliminar;
+        private System.Windows.Forms.Button bmodificar;
+        private System.Windows.Forms.Button bentrar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bbuscar;
+        private System.Windows.Forms.Button bsalir;
+        private System.Windows.Forms.TextBox txtbuscar;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.TextBox txtclave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bnuevo;
+        private System.Windows.Forms.ComboBox lstnivel;
+        private System.Windows.Forms.Button bactualizar;
+        private System.Windows.Forms.Button bprimero;
+        private System.Windows.Forms.Button banterior;
+        private System.Windows.Forms.Button bsiguiente;
+        private System.Windows.Forms.Button bultimo;
+    }
+}
+
---
+++ Segundo Parcial/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Segundo_Parcial
+{
+    public partial class Form1 : Form
+    {
+        public string cadena_conexion = "Database = sistemapro1; DataSource = localhost; User Id = Joan; Password = 12902";
+        public string usuario_modificar;
+        public string usuario_eliminar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtusuario.Enabled = false;
+            txtclave.Enabled = false;
+            lstnivel.Enabled = false;
+            try
+            {
+                String consulta = "Select * from usuarios ";
+                MySqlConnection conexion = new MySqlConnection(cadena_conexion);
+                MySqlDataAdapter comando = new MySqlDataAdapter(consulta, conexion);
+
+                System.Data.DataSet ds = new System.Data.DataSet();
+                comando.Fill(ds,"sistemapro1");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "sistemapro1";
+
+            }
+
+            catch
+            {
+                MessageBox.Show("Error de Conexion","Error!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+            }
+
+        }
+
+        private void bnuevo_Click(object sender, EventArgs e)
+        {
+            txtusuario.Enabled = true;
+            txtclave.Enabled = true;
+            lstnivel.Enabled = true;
+            txtusuario.Text = "";
+            txtclave.Text = "";
+            lstnivel.Text = "Seleccione nivel";
+            txtusuario.Focus();
+            bnuevo.Visible = false;
+            bguardar.Visible = true;
+        }
+
+        private void bguardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection myConnection = new MySqlConnection(cadena_conexion);
+                string myInsertQuery = "INSERT INTO usuarios (nombre,clave,nivel) Values(?nombre,?clave,?nivel)";
+                MySqlCommand myCommand = new MySqlCommand(myInsertQuery);
+                myCommand.Parameters.Add("?nombre", MySqlDbType.VarChar, 75).Value = txtusuario.Text;
+                myCommand.Parameters.Add("?clave", MySqlDbType.VarChar, 75).Value = txtclave.Text;
+                myCommand.Parameters.Add("?nivel", MySqlDbType.Int32, 11).Value = lstnivel.Text;
+
+
+                myCommand.Connection = myConnection;
+                myConnection.Open();
+                myCommand.ExecuteNonQuery();
+                myCommand.Connection.Close();
+                MessageBox.Show("Usuario agregado con éxito", "Ok", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                string consulta = "select * from usuarios";
+
+                MySqlConnection conexion = new MySqlConnection(cadena_conexion);
+                MySqlDataAdapter da = new MySqlDataAdapter(consulta, conexion);
+                System.Data.DataSet ds = new System.Data.DataSet();
+                da.Fill(ds, "sistemapro1");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "sistemapro1";
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("No se pudo guardar el usuario en la base de datos", "Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            bnuevo.Visible = true;
+            bguardar.Visible = false;
+            txtusuario.Enabled = false;
+            txtclave.Enabled = false;
+            lstnivel.Enabled = false;
+            bnuevo.Focus();
+        }
+
+        private void bmodificar_Click(object sender, EventArgs e)
+        {
+            txtusuario.Enabled = true;
+            txtclave.Enabled = true;
+            lstnivel.Enabled = true;
+            txtusuario.Focus();
+
+            bmodificar.Visible = false;
+            bactualizar.Visible = true;
+            usuario_modificar = txtusuario.Text.ToString();
+        }
+
+        private void bactualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection myConnection = new MySqlConnection(cadena_conexion);
+                string nom = txtusuario.Text.ToString();
+                string cla = txtclave.Text.ToString();
+                string niv = lstnivel.Text;
+
+                string myInsertQuery = "UPDATE usuarios SET nombre = '" + nom + "',clave = '" + cla + "',nivel = '" + niv + "' WHERE nombre = '" + usuario_modificar +"'";
+
+                MySqlCommand myCommand = new MySqlCommand(myInsertQuery);
+                myCommand.Connection = myConnection;
+                myConnection.Open();
+                myCommand.ExecuteNonQuery();
+                myCommand.Connection.Close();
+                MessageBox.Show("Usuario Actualizado con éxito", "Ok",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+                string consulta = "select * from usuarios";
+                
+                MySqlConnection conexion = new MySqlConnection(cadena_conexion);
+                MySqlDataAdapter da = new MySqlDataAdapter(consulta, conexion);
+                System.Data.DataSet ds = new System.Data.DataSet();
+                da.Fill(ds, "sistemapro1");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "sistemapro1";
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("Error al Actualizar el usuario", "Alerta!",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            bmodificar.Visible = true;
+            bactualizar.Visible = false;
+
+            txtusuario.Enabled = false;
+            txtclave.Enabled = false;
+            lstnivel.Enabled = false;
+            bmodificar.Focus();
+        }
+
+
+        private void bbuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection myConnection = new MySqlConnection(cadena_conexion);
+                string myInsertQuery = "select * from usuarios where idusuario = " + txtbuscar.Text + "";
+                MySqlCommand myCommand = new MySqlCommand(myInsertQuery, myConnection);
+                myConnection.Open();
+                MySqlDataReader myReader;
+                myReader = myCommand.ExecuteReader();
+                if (myReader.Read())
+                {
+
+                    txtusuario.Text = (myReader.GetString(1));
+                    txtclave.Text = (myReader.GetString(2));
+                    lstnivel.Text = (myReader.GetString(3));
+
+                }
+                else
+                {
+                    MessageBox.Show("El usuario no existe", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                myReader.Close();
+                myConnection.Close();
+
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("Campo de busqueda esta vasio", "Alerta!",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+
+            bnuevo.Visible = true;
+            bguardar.Visible = false;
+
+            txtusuario.Enabled = false;
+            txtclave.Enabled = false;
+            lstnivel.Enabled = false;
+            bmodificar.Focus(); 
+        }
+
+        private void beliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection myConnection = new MySqlConnection(cadena_conexion);
+                string myInsertQuery = "delete from usuarios Where idusuario = " + txtbuscar.Text + "";
+                MySqlCommand myCommand = new MySqlCommand(myInsertQuery);
+                myCommand.Connection = myConnection;
+                myConnection.Open();
+                myCommand.ExecuteNonQuery();
+                myCommand.Connection.Close();
+                MessageBox.Show("Usuario eliminado con éxito", "Ok",MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                string consulta = "select * from usuarios";
+
+                MySqlConnection conexion = new MySqlConnection(cadena_conexion);
+                MySqlDataAdapter da = new MySqlDataAdapter(consulta, conexion);
+                System.Data.DataSet ds = new System.Data.DataSet();
+                da.Fill(ds, "sistemapro1");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "sistemapro1";
+            }
+            catch
+            {
+                MessageBox.Show("Error al eliminar el usuario, primero realice búsqueda del usuario y después puede eliminar.", "Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            bnuevo.Visible = true;
+            bguardar.Visible = false;
+           
+            txtusuario.Enabled = false;
+            txtclave.Enabled = false;
+            lstnivel.Enabled = false;
+            txtusuario.Text = "";
+            txtclave.Text = "";
+            lstnivel.Text = "Seleccione nivel";
+            txtbuscar.Focus();
+        }
+
+        private void bsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bsiguiente_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+    }
+
